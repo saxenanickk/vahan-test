87e0603db73adee1e38ec6eb59a18155c2f2fe6a
Nikhil Saxena <saxenanickk@gmail.com>
2020-07-16 01:38:38 +0530

    Tabs & Tab component added

---
+++ src/components/Tab/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
+
+class Tab extends Component {
+  static propTypes = {
+    activeTab: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
+
+  onClick = () => {
+    const { label, onClick } = this.props;
+    onClick(label);
+  };
+
+  render() {
+    const {
+      onClick,
+      props: { activeTab, label },
+    } = this;
+
+    let className = "tab-list-item";
+
+    if (activeTab === label) {
+      className += " tab-list-active";
+    }
+
+    return (
+      <li className={className} onClick={onClick}>
+        <h5>{label}</h5>
+      </li>
+    );
+  }
+}
+
+export default Tab;
